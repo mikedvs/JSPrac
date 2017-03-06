28a4c4b73dfceb7210967bc671e6a159a8d5364d
Maliniak, Michael <Michael.Maliniak@news.com.au>
2017-03-06 11:29:02 +1100

    added simple dynamic text to basically indicate that javascript is loading..beats a console alert

---
+++ dynamic.js
@@ -0,0 +1,34 @@
+// An overly complicated way of getting some on the page!
+
+// IIFE ton initiate
+
+(function insertText(){
+
+    // text A
+
+    var textInserts = ['Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds.', 'Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds.'];
+
+    //Reverse Array
+
+    textInserts.reverse();
+
+    // create container for text;
+    var vessel = document.createElement('div');
+
+    // find heading component
+    var heading = document.querySelector('h1');
+
+    // for each item
+    for (var i = 0, len = textInserts.length; i < len; i++) {
+        var paragraph = document.createElement('p');
+        paragraph.textContent = textInserts[i];
+        insertAfter(heading, paragraph);
+    }
+
+    // function to insert items after a specific Node
+
+    function insertAfter(referenceNode, newNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+
+})();
--- prototypes.js
+++ prototypes.js
@@ -5,3 +5,10 @@
 //  - flexible
 //  - extensible
 //  - easy to understand
+
+// Everything is an Object (or a primitive)
+
+var a = {};
+var b = function() {};
+var c = [];
+
